4941604d78ec77b162922cb28ce86392c2f808a3
Shane <me@shaneis.me>
2014-02-27 09:47:43 -0800

    Fading pages using popstate
    Browser support leaves a lot to be desired, so I'll have to code in some
    fallbacks

--- src/js/framework/fading-pages.js
+++ src/js/framework/fading-pages.js
@@ -6,26 +6,19 @@ $(document).ready(function() {
   // First nav should also be active
   $('.fp-nav:first').addClass('active');
 
-  $('.fp-nav').click(function() {
-    if (!$(this).hasClass('active')) {
-      $('.fp-nav.active').removeClass('active');
-      $(this).addClass('active');
-      fadingPages($(this).attr('href'));
-    }    
-  });
-
   var top_z_index = 99; // optionally used to align content
-  var hash_init = window.location.hash; // check if there's a hash on page load
 
-  if (hash_init.length > 0) { hashChange(hash_init); }
+  window.addEventListener('popstate', function()  {
+    hashChange(window.location.hash);
+  });
   
   function hashChange(url_hash) {
     var hash_array = $('.fp-nav').map(function() { // build an array of all the .fp-nav links to check
       return $(this).attr('href');
     }).toArray();
     if (isInArray(hash_array,url_hash)) { // make sure the hash is in the array before continuing
-      var first_fp_nav_href = $('.fp-nav:first').attr('href'); // if hash_init is the same as this href, we don't do anything
-      if (url_hash !== first_fp_nav_href) {
+      var current_fp_nav_href = $('.fp-nav.active').attr('href');
+      if (url_hash !== current_fp_nav_href) {
         $('.fp-nav.active').removeClass('active');
         $(a[href=url_hash]).addClass('active');
         fadingPages(url_hash);
@@ -49,7 +42,7 @@ $(document).ready(function() {
   }
 
   // Simple check if something is inside an array
-  function isInArray(array,value) {
+  function isInArray(array,value) {o
     for (var i = array.length; i--;) {
       if (value.indexOf(array[i]) > -1) return true;
     }

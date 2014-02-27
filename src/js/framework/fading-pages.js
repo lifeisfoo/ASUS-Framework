aa893c101ca613a176de4e6883d8947155c4191e
Shane <me@shaneis.me>
2014-02-26 17:27:07 -0800

    Check hash on page load
    * Will build array of hash values on the page
    * Will check against this array for the hash_init
    * Will also double check it's not the same as the first since the page
    defaults there

--- src/js/framework/fading-pages.js
+++ src/js/framework/fading-pages.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
-  var top_z_index = 99;
-
+  var top_z_index = 99; // optionally used to align content
+  var hash_init = window.location.hash; // check if there's a hash on page load
+  var hash_array = $('.fp-nav').map(function() { // build an array of all the .fp-nav links to check
+    return $(this).attr('href');
+  }).toArray();
+  
   // Hide all except for the first parent
   $('.fp-content-parent:not(:first)').addClass('off');
   $('.fp-content-parent:first').addClass('on');
@@ -15,6 +19,15 @@ $(document).ready(function() {
       fadingPages($(this).attr('href'));
     }    
   });
+  
+  if (isInArray(hash_array,hash_init)) { // make sure the hash is in the array before continuing
+    var first_fp_nav_href = $('.fp-nav:first').attr('href'); // if hash_init is the same as this href, we don't do anything
+    if (hash_init.length > 0 && hash_init !== first_fp_nav_href) {
+      $('.fp-nav.active').removeClass('active');
+      $(a[href=hash_init]).addClass('active');
+      fadingPages(hash_init);
+    }
+  }
 
   function fadingPages(id) {    
 
@@ -31,6 +44,14 @@ $(document).ready(function() {
 
   function toggleFadeState(context) {
     $(context).toggleClass('on').toggleClass('off').removeAttr('style');
+  }
+
+  // Simple check if something is inside an array
+  function isInArray(array,value) {
+    for (var i = array.length; i--;) {
+      if (value.indexOf(array[i]) > -1) return true;
+    }
+    return false;
   }
 
 });

3e6c532f553f43eb08c5469c4d38a24ebff6e2ce
Shane <me@shaneis.me>
2014-02-28 13:41:02 -0800

    Basic content switching

--- src/js/framework/tabbed-content.js
+++ src/js/framework/tabbed-content.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
   // First nav should also be active per container
   $('.tc-container').find('.tc-nav:first').addClass('active');
 
+  $('.tc-nav').click(function(e) {
+    e.preventDefault();
+    if (!$(this).hasClass('active')) {
+      $(this).addClass('active');
+      $(this).parent().siblings().children('.active').removeClass('active');
+      switchContent($(this).attr('href'));
+    }
+  });
+
+  function switchContent(id) {
+    $(id).parent().children('.tc-content-parent.on').toggleClass('off').toggleClass('on');
+    $(id).toggleClass('off').toggleClass('on');
+  }
+
 });

dc56a876c68e9187ae56134d51990fd7d5e52df9
Shane <me@shaneis.me>
2014-03-11 15:18:21 -0700

    make jshint happy

--- src/js/framework/scroll-hijack.js
+++ src/js/framework/scroll-hijack.js
@@ -1,16 +1,13 @@
+$(document).ready(function() {
 
-  <script type="text/javascript">
-  $(document).ready(function() {
+  var page_height = $('.fp-content-parent.on').height();
+  var vp_height = $(window).height();
 
-    var page_height = $(".fp-content-parent.on").height(),
-        vp_height = $(window).height();
+  if (page_height <= vp_height) {
+    console.log('Scroll hijack');
+  }
 
-    if (page_height <= vp_height) {
-      console.log('Scroll hijack');
-    }
+  console.log('page: ' + page_height);
+  console.log('vp: ' + vp_height);
 
-    console.log('page: ' + page_height);
-    console.log('vp: ' + vp_height);
-
-  });
-  </script>  
+});

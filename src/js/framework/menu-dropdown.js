68fd43ebdbffd8a031f7eab2cd8f20f722cbe5ce
Shane <me@shaneis.me>
2014-03-11 17:25:49 -0700

    Close when anything else is clicked

--- src/js/framework/menu-dropdown.js
+++ src/js/framework/menu-dropdown.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
 
-  $('.dropdown-menu-button').click(function(e) {
+  var $menu_button = $('.dropdown-menu-button');
+
+  $menu_button.on('click', function(e) {
     e.preventDefault();
     toggleMenu();
   });
 
-  $('.dropdown-item > a').click(function() {
-    toggleMenu();
-  })
+  $(document).on('click', function(e) {
+    $.each( $menu_button, function() {
+ 
+      var $el = $(this);
+
+      if (this !== e.target && !$el.has(e.target).length && !$(e.target).is('.dropdown-menu-button') && $('.dropdown-menu').is(':visible')) { 
+        toggleMenu();
+      }
+
+    });
+  });
 
   function toggleMenu() {
     $('.dropdown-menu').slideToggle('400','easeOutCirc');

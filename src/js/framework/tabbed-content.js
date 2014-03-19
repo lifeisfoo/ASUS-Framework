2ec202e88986965e8eb052789d37195813e41662
Shane <me@shaneis.me>
2014-03-19 12:06:17 -0700

    Fire only if tc-container exists

--- src/js/framework/tabbed-content.js
+++ src/js/framework/tabbed-content.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 
-	// Hide all except for the first parent for each container
-  $('.tc-container').find('>.tc-content-parent:not(:first)').addClass('off');
-  $('.tc-container').find('>.tc-content-parent:first').addClass('on');
-  // First nav should also be active per container
-  $('.tc-container').find('.tc-nav:first').addClass('active');
+  if ($('.tc-container').length) {
+    // Hide all except for the first parent for each container
+    $('.tc-container').find('>.tc-content-parent:not(:first)').addClass('off');
+    $('.tc-container').find('>.tc-content-parent:first').addClass('on');
+    // First nav should also be active per container
+    $('.tc-container').find('.tc-nav:first').addClass('active');
 
-  $('.tc-nav').on('click touchstart', function(e) {
-    e.preventDefault();
-    if (!$(this).hasClass('active')) {
-      $(this).addClass('active');
-      $(this).parent().siblings().children('.active').removeClass('active');
-      switchContent($(this).attr('href'));
+    $('.tc-nav').on('click touchstart', function(e) {
+      e.preventDefault();
+      if (!$(this).hasClass('active')) {
+        $(this).addClass('active');
+        $(this).parent().siblings().children('.active').removeClass('active');
+        switchContent($(this).attr('href'));
+      }
+    });
+
+    function switchContent(id) {
+      $(id).parent().children('.tc-content-parent.on').toggleClass('off').toggleClass('on');
+      $(id).toggleClass('off').toggleClass('on');
     }
-  });
-
-  function switchContent(id) {
-    $(id).parent().children('.tc-content-parent.on').toggleClass('off').toggleClass('on');
-    $(id).toggleClass('off').toggleClass('on');
   }
 
 });

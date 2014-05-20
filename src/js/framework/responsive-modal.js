46567705a03ff12dbe833c2ad8d4568222f26c7e
Shane <me@shaneis.me>
2014-05-20 14:40:36 -0700

    Declare functions before being called

--- src/js/framework/responsive-modal.js
+++ src/js/framework/responsive-modal.js
@@ -3,6 +3,54 @@ $(document).ready(function() {
   var overlay = $('<div class="modal-overlay"></div>');
   var modal_box = $('<div class="modal-box"></div>');
 
+  function window_width() {
+    return $(window).width();
+  }
+
+  function document_height() {
+    return $(document).height();
+  }
+
+  function reset_default() {
+    $('body').removeClass('cancel-overflow');
+  }
+
+  function add_modal_box() {
+    $(modal_box).appendTo(overlay);
+  }
+
+  function close_modal() {
+    reset_default();
+    $(overlay).fadeOut().detach();
+    $(modal_box).empty();
+  }
+
+  function add_overlay() {
+    $(overlay).appendTo('body').fadeIn('slow', function() {
+      add_modal_box();
+    });
+
+    $('.modal-overlay').on('click touchstart', function() {
+      close_modal();
+    });
+  }
+
+  function add_youtube(id) {
+    var video_container = $('<div class="video-container"><iframe src="http://www.youtube.com/embed/'+ id +'?autoplay=1&rel=0" frameborder="0" width="560" height="315"></iframe></div>');
+
+    $(video_container).appendTo(modal_box);
+  }
+
+  function get_youtube_id(url) {
+    var id = url.split('v=')[1];
+    var amp = id.indexOf('&');
+    return (amp != -1) ? id.substring(0, amp) : id;
+  }
+
+  function stop_overflow() {
+    $('body').addClass('cancel-overflow');
+  }
+
   $('.modal').on('click touchstart', function(e) {
 
     e.preventDefault();
@@ -24,53 +72,4 @@ $(document).ready(function() {
 
   });
 
-  function window_width() {
-    return $(window).width();
-  }
-
-  function document_height() {
-    return $(document).height();
-  }
-
-  function add_overlay() {
-    $(overlay).appendTo('body').fadeIn('slow', function() {
-      add_modal_box();
-    });
-
-    $('.modal-overlay').on('click touchstart', function() {
-      close_modal();
-    });
-  }
-
-  function close_modal() {
-    reset_default();
-    $(overlay).fadeOut().detach();
-    $(modal_box).empty();
-  }
-
-  function add_modal_box() {
-    $(modal_box).appendTo(overlay);
-  }
-
-  function add_youtube(id) {
-    var video_container = $('<div class="video-container"><iframe src="http://www.youtube.com/embed/'+ id +'?autoplay=1&rel=0" frameborder="0" width="560" height="315"></iframe></div>');
-
-    $(video_container).appendTo(modal_box);
-  }
-
-  function get_youtube_id(url) {
-    var id = url.split('v=')[1];
-    var amp = id.indexOf('&');
-    return (amp != -1) ? id.substring(0, amp) : id;
-  }
-
-  function stop_overflow() {
-    $('body').addClass('cancel-overflow');
-  }
-
-  function reset_default() {
-    $('body').removeClass('cancel-overflow');
-  }
-
 });
-
